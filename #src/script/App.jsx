4482e732429a75ed29fcff96a8006bc277a3fdba
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-02-15 20:37:37 +0200

    feat: add links for component

--- #src/script/App.jsx
+++ #src/script/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Maim from './component/Main';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
